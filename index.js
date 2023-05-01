69a1b38207b6ecc4a7293762d31cdcf38b57f305
EmranAli-15 <mohammademranali15@gmail.com>
2023-05-02 02:21:24 +0600

    cors added

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 5000
 const chef = require('./data/chef.json')
+const cors = require('cors')
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
